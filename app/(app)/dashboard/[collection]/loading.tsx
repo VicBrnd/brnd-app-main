3ed986b610876d76426bb8b254942110b21d0ab7
Victor <30707373+VicBrnd@users.noreply.github.com>
2026-02-07 16:17:42 +0100

    feat: réorganiser la structure du composant de chargement pour améliorer la mise en page

--- app/(app)/dashboard/[collection]/loading.tsx
+++ app/(app)/dashboard/[collection]/loading.tsx
@@ -6,19 +6,22 @@ export default function CollectionLoading() {
   return (
     <>
       <CollectionHeaderSkeleton />
-      <div className="flex flex-col gap-2">
-        <h2 className="text-sm font-medium text-muted-foreground">Documents</h2>
-        <Page
-          title="Overview"
-          description="View, edit, and manage the documents in this collection"
-        >
+      <Page
+        title="Overview"
+        description="View, edit, and manage the documents in this collection"
+      >
+        <div className="flex flex-col gap-2">
+          <h2 className="text-sm font-medium text-muted-foreground">
+            Documents
+          </h2>
+
           <DataTableSkeleton
             columnCount={5}
             withViewOptions={false}
             withPagination={false}
           />
-        </Page>
-      </div>
+        </div>
+      </Page>
     </>
   );
 }
