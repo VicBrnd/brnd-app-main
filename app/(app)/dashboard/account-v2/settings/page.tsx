36ceb35ddd6dbaa44fef2ad1141cced9b17fe434
Victor <30707373+VicBrnd@users.noreply.github.com>
2026-02-07 16:06:11 +0100

    feat: ajouter la page de paramètres de compte avec un composant Page pour la gestion des paramètres

---
+++ app/(app)/dashboard/account-v2/settings/page.tsx
@@ -0,0 +1,7 @@
+import { Page } from "@/components/page-layout";
+
+export default function AccountSettingsPageV2() {
+  <Page title="General" description="Manage your account settings">
+    Hellos
+  </Page>;
+}
